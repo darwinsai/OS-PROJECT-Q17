4f85d4cee41e4709384f145e2ab8b06332a58e84
darwinsai <38688596+darwinsai@users.noreply.github.com>
2018-04-28 18:20:08 +0530

    Add files via upload

---
+++ darwin.cpp
@@ -0,0 +1,189 @@
+#include<iostream>
+using namespace std;
+struct process
+{
+	int at,bt,id,wtime;
+	bool exe;
+}s[10];
+void sort(process** , int );
+void reversesort(process** , int );
+void sjf(process* , int );
+int main()
+{
+	int n,clock;
+	cout<<"Implementing Longest Job First\n\n";
+	cout<<"Enter Number of process: ";
+	cin>>n;
+	//Intializing
+	for(int i=0;i<n;i++)
+	{
+		cout<<"Enter the arrival time for process "<<i+1<<":";
+		cin>>s[i].at;
+		s[i].id=i+1;
+		s[i].bt=s[i].at*2;
+		s[i].exe=false;
+		s[i].wtime=0;
+	}
+	//display
+	cout<<"Details of processes are: \n";
+	cout<<"ID\tArrival Time\tBurst Time\n";
+	for(int i=0;i<n;i++)
+	{
+		cout<<s[i].id<<"\t"<<s[i].at<<"\t\t"<<s[i].bt;
+		cout<<"\n";
+	}
+
+	process s1[n],*readyqueue[n];
+	int size=0;
+	//sorting
+	clock=0;
+	process *executing = NULL;
+	while(true)
+	{
+		bool alldone=true;
+		for(int i=0;i<n;i++)
+		{
+			if(s[i].exe==false)
+			{
+				alldone=false;
+				break;
+			}
+		}	
+		if(alldone)
+			break;
+		for(int i=0;i<n;i++)
+		{
+			if(s[i].at==clock && s[i].exe==false){
+				readyqueue[size]=&s[i];
+				size++;
+			}
+		}
+		if ( executing != NULL && executing->bt + executing->wtime + executing->at == clock) {
+			executing = NULL;
+		}
+		if(size>0 && executing == NULL)
+		{
+			sort(readyqueue,size);
+			readyqueue[size-1]->exe=true;
+			executing = readyqueue[size-1];
+			cout<<readyqueue[size-1]->id<<" executed"<<endl;
+			size--;
+		}
+		for(int j=0;j<size;j++)
+		{
+			readyqueue[j]->wtime++;
+		}
+		clock++;
+	}
+	int totalcomplete=0;
+	int totalturnaround=0;
+	int totalwaiting=0;
+	for(int i=0;i<n;i++)
+	{
+		totalwaiting += s[i].wtime;
+		totalcomplete=s[i].bt+s[i].wtime+s[i].at+totalcomplete;
+		totalturnaround=(s[i].bt+s[i].wtime)+totalturnaround;
+	}
+	cout<<"Total Complete Time: "<<totalcomplete<<endl;
+	cout<<"Total Turn Around Time: "<<totalturnaround<<endl;
+	cout<<"Average Turn Around Time: "<<(float)(totalturnaround)/n<<endl;
+	cout<<"Average Waiting Time: "<<(float)(totalwaiting)/n<<endl;
+	for(int i=0;i<n;i++)
+	{
+		s[i].wtime=0;
+		s[i].exe=false;
+	}
+	cout<<"\n\nImplementing Shortest Jop First\n\n";
+	sjf(s,n);
+}
+
+void sort(process *readyqueue[10],int n)
+{
+	for(int i=0;i<n-1;i++)
+	{
+		for(int j=0;j<n-i-1;j++)
+		{
+			if(readyqueue[j]->bt>readyqueue[j+1]->bt)
+			{
+				process *temp=readyqueue[j];
+				readyqueue[j]=readyqueue[j+1];
+				readyqueue[j+1]=temp;
+			}
+		}
+	}
+}
+void reversesort(process *readyqueue[10],int n)
+{
+	for(int i=0;i<n-1;i++)
+	{
+		for(int j=0;j<n-i-1;j++)
+		{
+			if(readyqueue[j]->bt<readyqueue[j+1]->bt)
+			{
+				process *temp=readyqueue[j];
+				readyqueue[j]=readyqueue[j+1];
+				readyqueue[j+1]=temp;
+			}
+		}
+	}
+}	
+void sjf(process *s,int n1)
+{
+	int n=n1;
+	process *readyqueue1[n];
+
+	int size=0;
+	//sorting
+	int clock=0;
+	process *executing = NULL;
+	while(true)
+	{
+		bool alldone=true;
+		for(int i=0;i<n;i++)
+		{
+			if(s[i].exe==false)
+			{
+				alldone=false;
+				break;
+			}
+		}	
+		if(alldone)
+			break;
+		for(int i=0;i<n;i++)
+		{
+			if(s[i].at==clock && s[i].exe==false){
+				readyqueue1[size]=&s[i];
+				size++;
+			}
+		}
+		if ( executing != NULL && executing->bt + executing->wtime + executing->at == clock) {
+			executing = NULL;
+		}
+		if(size>0 && executing == NULL)
+		{
+			reversesort(readyqueue1,size);
+			readyqueue1[size-1]->exe=true;
+			executing = readyqueue1[size-1];
+			cout<<readyqueue1[size-1]->id<<" executed"<<endl;
+			size--;
+		}
+		for(int j=0;j<size;j++)
+		{
+			readyqueue1[j]->wtime++;
+		}
+		clock++;
+	}
+	int totalcomplete=0;
+	int totalturnaround=0;
+	int totalwaiting=0;
+	for(int i=0;i<n;i++)
+	{
+		totalwaiting += s[i].wtime;
+		totalcomplete=s[i].bt+s[i].wtime+s[i].at+totalcomplete;
+		totalturnaround=(s[i].bt+s[i].wtime)+totalturnaround;
+	}
+	cout<<"Total Complete Time: "<<totalcomplete<<endl;
+	cout<<"Total Turn Around Time: "<<totalturnaround<<endl;
+	cout<<"Average Turn Around Time: "<<(float)(totalturnaround)/n<<endl;
+	cout<<"Average Waiting Time: "<<(float)(totalwaiting)/n<<endl;
+}
